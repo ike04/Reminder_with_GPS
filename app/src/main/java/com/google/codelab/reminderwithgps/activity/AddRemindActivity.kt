fdb92f9b09e20b0defb9a4243d8a170b30cae8d2
ike04 <boc.0409@outlook.jp>
2021-02-15 18:14:40 +0900

    現在地/ピンを設定

--- app/src/main/java/com/google/codelab/reminderwithgps/activity/AddRemindActivity.kt
+++ app/src/main/java/com/google/codelab/reminderwithgps/activity/AddRemindActivity.kt
@@ -1,21 +1,133 @@
 package com.google.codelab.reminderwithgps.activity
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.codelab.reminderwithgps.R
 
-class AddRemindActivity : AppCompatActivity() {
+class AddRemindActivity : AppCompatActivity(), OnMapReadyCallback , GoogleMap.OnMapLongClickListener{
+    private lateinit var mMap: GoogleMap
+    private val MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 1
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
+    private var locationCallback: LocationCallback? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_remind)
 
         supportActionBar?.setTitle(R.string.add_remind)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        checkPermission()
+        mMap.setOnMapLongClickListener(this)
+    }
+
+    private fun checkPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            myLocationEnable()
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // 許可を求め、拒否されていた場合
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION
+            )
+        } else {
+            // まだ許可を求めていない
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION -> {
+                if (permissions.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 許可された
+                    myLocationEnable()
+                } else {
+                    Toast.makeText(this, "現在地は表示できません", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private fun myLocationEnable() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            mMap.isMyLocationEnabled = true
+            val locationRequest = LocationRequest().apply {
+                interval = 10000
+                fastestInterval = 5000
+                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            }
+            locationCallback = object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult?) {
+                    super.onLocationResult(locationResult)
+                    if (locationResult?.lastLocation != null) {
+                        lastLocation = locationResult.lastLocation
+                        val currentLatLng = LatLng(lastLocation.latitude, lastLocation.longitude)
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng))
+                    }
+                }
+            }
+            fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                null
+            )
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -51,5 +163,11 @@ class AddRemindActivity : AppCompatActivity() {
             InputMethodManager.HIDE_NOT_ALWAYS
         )
         return false
+    }
+
+    override fun onMapLongClick(point: LatLng) {
+        mMap.clear()
+        val marker = MarkerOptions().position(point).title("リマインド箇所")
+        mMap.addMarker(marker)
     }
 }
