9d5fdbf3b7e6b1bbcbb15ece58b4bbae8f0f8b77
ike04 <boc.0409@outlook.jp>
2021-03-21 11:34:56 +0900

    ログイン機能の実装

--- app/src/main/java/com/google/codelab/reminderwithgps/activity/LoginActivity.kt
+++ app/src/main/java/com/google/codelab/reminderwithgps/activity/LoginActivity.kt
@@ -3,21 +3,32 @@ package com.google.codelab.reminderwithgps.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.codelab.reminderwithgps.R
 import com.google.codelab.reminderwithgps.utils.ValidationUtils
 import com.google.codelab.reminderwithgps.utils.showAlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private val TAG = this::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         supportActionBar?.setTitle(R.string.login)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
         val loginBtn = findViewById<Button>(R.id.loginBtn)
         val signInBtn = findViewById<Button>(R.id.signInBtn)
@@ -29,9 +40,7 @@ class LoginActivity : AppCompatActivity() {
             val errorMessage = ValidationUtils.checkLogin(email, password)
 
             if (errorMessage == null) {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
+               login(email, password)
             } else {
                 showAlertDialog(R.string.warning, errorMessage)
             }
@@ -41,6 +50,27 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
