54c75e4595929181496bad927606adbd6d3fb827
ike04 <boc.0409@outlook.jp>
2021-03-21 11:44:16 +0900

    サインアップ機能の実装

--- app/src/main/java/com/google/codelab/reminderwithgps/activity/LoginActivity.kt
+++ app/src/main/java/com/google/codelab/reminderwithgps/activity/LoginActivity.kt
@@ -58,7 +58,7 @@ class LoginActivity : AppCompatActivity() {
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "signInWithEmail:success")
-                    
+
                     val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
                     finish()
--- app/src/main/java/com/google/codelab/reminderwithgps/activity/SignInActivity.kt
+++ app/src/main/java/com/google/codelab/reminderwithgps/activity/SignInActivity.kt
@@ -3,17 +3,25 @@ package com.google.codelab.reminderwithgps.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.codelab.reminderwithgps.R
 import com.google.codelab.reminderwithgps.utils.ValidationUtils
 import com.google.codelab.reminderwithgps.utils.showAlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private val TAG = this::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
@@ -22,6 +30,9 @@ class SignInActivity : AppCompatActivity() {
             it.setTitle(R.string.sign_up)
             it.setDisplayHomeAsUpEnabled(true)
         }
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -40,9 +51,7 @@ class SignInActivity : AppCompatActivity() {
                 val errorMessage = ValidationUtils.checkSignUp(email, password, passwordConfirm)
 
                 if (errorMessage == null) {
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
+                    signUp(email, password)
                 } else {
                     showAlertDialog(R.string.warning, errorMessage)
                 }
@@ -55,6 +64,27 @@ class SignInActivity : AppCompatActivity() {
             }
         }
         return false
+    }
+
+    private fun signUp(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
